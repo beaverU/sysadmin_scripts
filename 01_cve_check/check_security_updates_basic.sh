db2f67d91d79cc583c9fbe4505eb2778b0b51e74
John Doe <johndoe8@duck.com>
2025-03-31 18:19:28 +0000

    New ed-script & some improvements in basic script

--- 01_cve_check/check_security_updates_basic.sh
+++ 01_cve_check/check_security_updates_basic.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Checks for Ubuntu/Debian security updates, identifies packages that
+# need to be upgraded
+
 VERBOSE=false
-TMP_FILE="/tmp/sec.cache"
 
 # syslog logging func
 log_message() {
@@ -16,30 +18,27 @@ cleanup() {
   fi
 }
 
-# Security updates check function
-check_security_updates() {
-  apt-get -s upgrade 2>/dev/null | grep -i security | grep -i Inst | cut -d ' ' -f 2 > "$TMP_FILE"
-  if (( PIPESTATUS[0] != 0 )); then
-    log_message "apt command has failed" "error"
-    "$VERBOSE" && echo "apt command has failed."
-    exit 1
-  elif [[ -z "$(cat $TMP_FILE)" ]]; then
-    log_message "All security updates are installed." "info"
-    "$VERBOSE" && echo "All security updates are installed."
-  else
-    log_message "Security updates has found." "warning"
-    "$VERBOSE" && echo "Security updates has found."
-  fi
-}
-
 # Checking script's args
 args_setup() {
-  while getopts "vh" opt; do
+  while getopts ":vh" opt; do
     case $opt in
       v) VERBOSE=true; echo "Verbose mode on" ;;
       *) echo "Usage: $0 [-v]"; exit 1 ;;
     esac
   done
+}
+
+# Security updates check function
+check_security_updates() {
+  local updates
+  updates=$(apt-get -s upgrade 2>/dev/null | grep -i security | grep -i Inst| cut -d ' ' -f 2-4 | sed 's/(//g')
+  if [[ -z "$updates" ]]; then
+    log_message "All security updates are installed." "info"
+    "$VERBOSE" && echo "All security updates are installed."
+  else
+    log_message "Security updates has found." "warning"
+    "$VERBOSE" && echo "Security updates has found." && echo "$updates"
+  fi
 }
 
 main() {
